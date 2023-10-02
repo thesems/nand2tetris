7690520c4d2a59a316b63161446c8f4fe2234efa
Semir Ramovic <semir.ramovic@outlook.com>
2023-10-02 20:30:44 +0200

    Refactored rust assembler into parser, code, symboltable structs.

--- assembler/src/lib.rs
+++ assembler/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs, str::FromStr};
+use std::{collections::HashMap, error::Error, fs, ops::Index, process};
 
 pub struct Config {
     pub in_file: String,
@@ -18,83 +18,273 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.in_file)?;
-    let lines = contents.split("\n");
+#[derive(Debug)]
+struct SymbolTable {
+    pub symbols: HashMap<String, u16>,
+    pub next_free_variable: u16,
+}
+impl SymbolTable {
+    pub fn new() -> SymbolTable {
+        let mut symbols: HashMap<String, u16> = HashMap::new();
+        for x in 0..16 {
+            symbols.insert(format!("R{x}"), x);
+        }
+        symbols.insert(String::from("SCREEN"), 16384);
+        symbols.insert(String::from("KBD"), 24576);
+        symbols.insert(String::from("SP"), 0);
+        symbols.insert(String::from("LCL"), 1);
+        symbols.insert(String::from("ARG"), 2);
+        symbols.insert(String::from("THIS"), 3);
+        symbols.insert(String::from("THAT"), 4);
+        // dbg!(symbols);
 
-    let mut out_lines: Vec<String> = Vec::new();
+        SymbolTable {
+            symbols: symbols,
+            next_free_variable: 16,
+        }
+    }
 
-    for line in lines {
-        if line.trim().is_empty() {
-            continue;
-        } else if line.starts_with("//") {
-            continue;
+    pub fn add_entry(&mut self, symbol: &String, address: u16) {
+        self.symbols.insert(symbol.clone(), address);
+    }
+
+    pub fn contains(&self, symbol: &String) -> bool {
+        return self.symbols.contains_key(symbol);
+    }
+
+    pub fn get_address(&self, symbol: &String) -> Option<&u16> {
+        return self.symbols.get(symbol.as_str());
+    }
+}
+
+#[derive(PartialEq)]
+enum CommandType {
+    ACommand = 1,
+    CComand = 2,
+    LCommand = 3,
+}
+
+struct Parser {
+    lines: Vec<String>,
+    current_line: i32,
+    instr_line: i32,
+    instructions: HashMap<i32, String>,
+}
+impl Parser {
+    pub fn build(in_file: &str) -> Result<Parser, Box<dyn Error>> {
+        let contents = fs::read_to_string(in_file)?;
+        let lines = contents.split("\n");
+        return Ok(Parser {
+            lines: lines
+                .into_iter()
+                .map(|line| String::from(line.trim()))
+                .filter(|line| !line.starts_with("//") && !line.is_empty())
+                .collect(),
+            current_line: -1,
+            instr_line: -1,
+            instructions: HashMap::new(),
+        });
+    }
+
+    pub fn reset_to_start(&mut self) {
+        self.current_line = -1;
+        self.instr_line = -1;
+    }
+
+    pub fn command_type(&self) -> CommandType {
+        let instr = self.instructions.get(&self.instr_line).unwrap();
+
+        if instr.starts_with("@") {
+            return CommandType::ACommand;
+        } else if instr.contains(";") || instr.contains("=") {
+            return CommandType::CComand;
+        } else if instr.contains("(") {
+            return CommandType::LCommand;
         }
 
-        println!("{line}");
+        panic!("invalid instruction type {instr}");
+    }
 
-        if line.starts_with("@") {
-            // A-Instruction
-            out_lines.push(handle_instruction_a(line.to_string().clone())?);
-        } else if line.contains("(") {
-            // Label
+    pub fn has_more_commands(&self, first_pass: bool) -> bool {
+        if (self.current_line + 1) as usize == self.lines.len() && first_pass {
+            return false;
+        } else if (self.instr_line + 1) as usize == self.instructions.len() && !first_pass {
+            return false;
+        }
+        return true;
+    }
+
+    pub fn advance(&mut self, st: &mut SymbolTable, first_pass: bool) {
+        if !self.has_more_commands(first_pass) {
+            return;
+        }
+
+        self.current_line += 1;
+        let mut line = self.lines.index(self.current_line as usize).trim();
+
+        if line.starts_with("//") || line.is_empty() {
+            self.advance(st, first_pass);
+            return;
+        }
+
+        let binding = self.clean_line(line);
+        line = binding.as_str();
+
+        if self.instr_line == -1 {
+            self.instr_line = 0;
         } else {
-            // C-Instruction
-            out_lines.push(handle_instruction_c(line.to_string().clone())?);
+            self.instr_line += 1;
+        }
+
+        if first_pass {
+            self.instructions
+                .insert(self.instr_line, String::from(line));
+        }
+
+        let command_type = self.command_type();
+        if first_pass && command_type == CommandType::LCommand {
+            let symbol = self.symbol();
+            if !st.contains(&symbol) {
+                st.add_entry(&symbol, self.instr_line as u16);
+            }
+            self.instr_line -= 1;
+        } else if !first_pass && command_type == CommandType::ACommand {
+            let symbol = self.symbol();
+            let mut is_num = true;
+            let num = symbol.trim().parse::<u16>().unwrap_or_else(|_| {
+                is_num = false;
+                0
+            });
+            if is_num {
+                st.add_entry(&symbol, num);
+            } else if !st.contains(&symbol) {
+                st.add_entry(&symbol, st.next_free_variable);
+                st.next_free_variable += 1;
+            }
+        }
+        // println!("{line}")
+    }
+
+    pub fn symbol(&self) -> String {
+        let instr = self.instructions.get(&self.instr_line).unwrap();
+        let size = instr.len();
+        if instr.contains("@") {
+            let value = &instr[1..size];
+            return String::from(value.trim());
+        } else if instr.contains("(") {
+            let value = &instr[1..size - 1];
+            return String::from(value.trim());
+        }
+
+        panic!("invalid instruction, not a symbol: {instr}")
+    }
+
+    pub fn dest(&self) -> String {
+        let instr = self.instructions.get(&self.instr_line).unwrap();
+        let tokens: Vec<&str> = instr.split("=").map(|val| val.trim()).collect();
+        if tokens.len() != 1 {
+            return String::from(tokens[0]);
+        }
+        return String::from("null");
+    }
+
+    pub fn comp(&self) -> String {
+        let instr = self.instructions.get(&self.instr_line).unwrap();
+
+        let tokens: Vec<&str>;
+        if instr.contains(";") {
+            // jump operation
+            tokens = instr.split(";").map(|val| val.trim()).collect();
+            return String::from(tokens[0]);
+        } else {
+            // assignment
+            tokens = instr.split("=").map(|val| val.trim()).collect();
+            return String::from(tokens[1]);
         }
     }
 
-    fs::write(
-        config.out_file,
-        out_lines
-            .iter()
-            .map(|line| format!("{}\n", line))
-            .collect::<Vec<String>>()
-            .concat(),
-    )?;
-
-    println!("Output hack binary:");
-    for line in out_lines {
-        println!("{line}")
-    }
-
-    Ok(())
-}
-
-fn handle_instruction_a(instr: String) -> Result<String, Box<dyn Error>> {
-    let size = instr.len();
-    let value = &instr[1..size];
-    let mut variable = "";
-
-    let mut is_num = true;
-    let num = value.trim().parse::<u16>().unwrap_or_else(|_| {
-        variable = value;
-        is_num = false;
-        0
-    });
-
-    if !is_num {
-        // variable
-        println!("Variable: {num}!");
-    } else {
-        // integer value
-        return Ok(format!("{num:016b}"));
-    }
-
-    // if number => binary
-    // else => save as variable
-    return Ok(String::from_str(value)?);
-}
-
-fn handle_instruction_c(instr: String) -> Result<String, Box<dyn Error>> {
-    if instr.contains(";") {
+    pub fn jump(&self) -> String {
+        let instr = self.instructions.get(&self.instr_line).unwrap();
         // jump operation
-        Ok(String::from("to-do jump"))
-    } else {
-        // assignment operation
-        let tokens: Vec<&str> = instr.split("=").map(|val| val.trim()).collect();
+        let tokens: Vec<&str> = instr.split(";").map(|val| val.trim()).collect();
 
-        let dest = match tokens[0] {
+        if tokens.len() == 1 {
+            // semi-colon not found
+            return String::from("null");
+        }
+
+        return String::from(tokens[1]);
+    }
+
+    pub fn clean_line(&self, line: &str) -> String {
+        let mut clean = line.trim();
+
+        let idx = match clean.find("//") {
+            Some(x) => x,
+            None => 123456,
+        };
+
+        if idx != 123456 {
+            clean = &clean[0..idx];
+        }
+
+        return String::from(clean);
+    }
+}
+
+struct Code {}
+impl Code {
+    fn new() -> Code {
+        return Code {};
+    }
+    pub fn comp(&self, mnemonic: &str) -> Result<String, Box<dyn Error>> {
+        let comp = match mnemonic {
+            "0" => "101010",
+            "1" => "111111",
+            "-1" => "111010",
+            "D" => "001100",
+            "A" => "110000",
+            "M" => "110000",
+            "!D" => "001101",
+            "!A" => "110001",
+            "!M" => "110001",
+            "-D" => "001111",
+            "-A" => "110011",
+            "-M" => "110011",
+            "D+1" => "011111",
+            "A+1" => "110111",
+            "M+1" => "110111",
+            "D-1" => "001110",
+            "A-1" => "110010",
+            "M-1" => "110010",
+            "D+A" => "000010",
+            "D+M" => "000010",
+            "D-A" => "010011",
+            "D-M" => "010011",
+            "A-D" => "000111",
+            "M-D" => "000111",
+            "D&A" => "000000",
+            "D&M" => "000000",
+            "D|A" => "010101",
+            "D|M" => "010101",
+            _ => "invalid",
+        };
+
+        if comp == "invalid" {
+            return Err("invalid computation {mnemonic}".into());
+        }
+
+        let mut result = String::from(comp);
+        if mnemonic.contains("M") {
+            result.insert(0, '1');
+        } else {
+            result.insert(0, '0');
+        }
+        return Ok(result);
+    }
+
+    pub fn dest(&self, mnemonic: &str) -> Result<String, Box<dyn Error>> {
+        let dest = match mnemonic {
             "null" => "000",
             "M" => "001",
             "D" => "010",
@@ -107,39 +297,81 @@ fn handle_instruction_c(instr: String) -> Result<String, Box<dyn Error>> {
         };
 
         if dest == "invalid" {
-            return Err("invalid destination".into());
+            return Err(format!("invalid destination: {mnemonic}").into());
         }
 
-        let comp = match tokens[1] {
-            "0" => "101010",
-            "1" => "111111",
-            "-1" => "111010",
-            "D" => "001100",
-            "A" => "110000",
-            "!D" => "001101",
-            "!A" => "110001",
-            "-D" => "001111",
-            "-A" => "110011",
-            "D+1" => "011111",
-            "A+1" => "110111",
-            "D-1" => "001110",
-            "A-1" => "110010",
-            "D+A" => "000010",
-            "D-A" => "010011",
-            "A-D" => "000111",
-            "D&A" => "000000",
-            "D|A" => "010101",
+        return Ok(String::from(dest));
+    }
+
+    fn jump(&self, mnemonic: &str) -> Result<String, Box<dyn Error>> {
+        let jump = match mnemonic {
+            "null" => "000",
+            "JGT" => "001",
+            "JEQ" => "010",
+            "JGE" => "011",
+            "JLT" => "100",
+            "JNE" => "101",
+            "JLE" => "110",
+            "JMP" => "111",
             _ => "invalid",
         };
-
-        if comp == "invalid" {
-            dbg!(tokens);
-            return Err("invalid computation".into());
+        if jump == "invalid" {
+            return Err(format!("invalid jump: {mnemonic}").into());
         }
-
-        let out = format!("1110{:#6}{:#3}000", comp, dest);
-        Ok(String::from(out))
+        return Ok(String::from(jump));
     }
 }
 
-fn handle_label() {}
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut parser = Parser::build(config.in_file.as_str()).unwrap();
+    let mut st = SymbolTable::new();
+
+    // Build symbol table
+    loop {
+        if !parser.has_more_commands(true) {
+            break;
+        }
+        parser.advance(&mut st, true);
+    }
+
+    // Reset parser
+    parser.reset_to_start();
+
+    // Compile program
+    let code = Code::new();
+    let mut out_lines: Vec<String> = Vec::new();
+    loop {
+        if !parser.has_more_commands(false) {
+            break;
+        }
+        parser.advance(&mut st, false);
+
+        if parser.command_type() == CommandType::CComand {
+            let comp = code.comp(&parser.comp()).unwrap();
+            let dest = code.dest(&parser.dest()).unwrap();
+            let jump = code.jump(&parser.jump()).unwrap();
+            out_lines.push(format!("111{:#7}{:#3}{:#3}", comp, dest, jump));
+        } else if parser.command_type() == CommandType::ACommand {
+            let symbol = parser.symbol();
+            let addr = st.get_address(&symbol).unwrap_or_else(|| {
+                println!("Error: undefined variable {symbol}");
+                process::exit(1);
+            });
+            out_lines.push(format!("{addr:016b}"));
+        } else if parser.command_type() == CommandType::LCommand {
+            continue;
+        }
+    }
+
+    // Write .hack file
+    fs::write(
+        config.out_file,
+        out_lines
+            .iter()
+            .map(|line| format!("{}\n", line))
+            .collect::<Vec<String>>()
+            .concat(),
+    )?;
+
+    Ok(())
+}
