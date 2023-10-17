8192a65406f4c21fe242fcfa33782720791071d0
Semir Ramovic <semir.ramovic@easelink.com>
2023-10-17 10:59:58 +0200

    split vmtranslator in 07 and 08 directories

---
+++ vmtranslator-08/src/main.rs
@@ -0,0 +1,16 @@
+use std::{env, process};
+use vmtranslator::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        println!("Program format: <input asm path> <output hack path>");
+        process::exit(1);
+    });
+
+    if let Err(e) = vmtranslator::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
