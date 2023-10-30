eb460f616a14804769e477f9130815b21b01b1d1
Semir Ramovic <semir.ramovic@easelink.com>
2023-10-30 11:11:08 +0100

    Simplify tokenizer tests.

--- 10/jackanalyzer/src/tests.rs
+++ 10/jackanalyzer/src/tests.rs
@@ -8,51 +8,26 @@ mod tests {
             let input = "let x = 4;\nlet y = -1;";
             let mut tokenizer = tokenizer::Tokenizer::build(input).unwrap();
             
-            println!("Testing input: {}", input); 
+            println!("Testing input: {}", input);
 
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Keyword);
-            assert_eq!(tokenizer.keyword(), tokenizer::KeywordType::Let);
+            let token_types = vec![
+                tokenizer::TokenType::Keyword,
+                tokenizer::TokenType::Identifier,
+                tokenizer::TokenType::Symbol,
+                tokenizer::TokenType::IntConst,
+                tokenizer::TokenType::Symbol,
+                tokenizer::TokenType::Keyword,
+                tokenizer::TokenType::Identifier,
+                tokenizer::TokenType::Symbol,
+                tokenizer::TokenType::Symbol,
+                tokenizer::TokenType::IntConst,
+                tokenizer::TokenType::Symbol,
+            ];
 
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Identifier);
-            assert_eq!(tokenizer.identifier(), "x");
-            
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Symbol);
-            assert_eq!(tokenizer.symbol(), '=');
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::IntConst);
-            assert_eq!(tokenizer.int_val(), 4);
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Symbol);
-            assert_eq!(tokenizer.symbol(), ';');
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Keyword);
-            assert_eq!(tokenizer.keyword(), tokenizer::KeywordType::Let);
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Identifier);
-            assert_eq!(tokenizer.identifier(), "y");
-            
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Symbol);
-            assert_eq!(tokenizer.symbol(), '=');
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Symbol);
-            assert_eq!(tokenizer.symbol(), '-');
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::IntConst);
-            assert_eq!(tokenizer.int_val(), 1);
-
-            tokenizer.advance();
-            assert_eq!(tokenizer.token_type(), tokenizer::TokenType::Symbol);
-            assert_eq!(tokenizer.symbol(), ';');
+            for token in token_types {
+                tokenizer.advance();
+                assert_eq!(tokenizer.token_type(), token);
+            }
         }
     }
 }
