5d168a323a8cf1cac611e1be15c69a5b1c8bc803
Semir Ramovic <semir.ramovic@outlook.com>
2023-11-01 19:54:27 +0100

    Implement compile functions for variable declarations and other.

--- 10/jackanalyzer/src/compilation_engine.rs
+++ 10/jackanalyzer/src/compilation_engine.rs
@@ -156,6 +156,7 @@ impl<'a> CompilationEngine<'a> {
     }
 
     pub fn compile_parameter_list(&mut self) {
+        self.xml_writer.write_full_tag("<parameterList>");
         self.tokenizer.advance();
         if self.tokenizer.token_type() != TokenType::Symbol || self.tokenizer.token != "(" {
             panic!(
@@ -175,6 +176,7 @@ impl<'a> CompilationEngine<'a> {
             );
         }
         self.write_token();
+        self.xml_writer.write_full_tag("</parameterList>");
     }
 
     pub fn compile_subroutine_body(&mut self) {
@@ -187,40 +189,143 @@ impl<'a> CompilationEngine<'a> {
         }
         self.write_token();
 
-        // TODO: handle parameters
+        self.compile_var_dec();
+        
+        self.xml_writer.write_full_tag("<statements>");
+        self.compile_statements();
+        self.xml_writer.write_full_tag("</statements>");
 
         self.tokenizer.advance();
         if self.tokenizer.token_type() != TokenType::Symbol || self.tokenizer.token != "}" {
             panic!(
-                "ERROR: function body must start with an opening curly bracket '}}', but received: {}",
+                "ERROR: function body must start with an closed curly bracket '}}', but received: {}",
                 self.tokenizer.token
             );
         }
         self.write_token();
     }
 
-    pub fn compile_var_dec() {}
+    pub fn compile_var_dec(&mut self) {
+        self.xml_writer.write_full_tag("<varDec>");
+        self.tokenizer.advance();
+        if self.tokenizer.token_type() != TokenType::Keyword || self.tokenizer.token != "var" {
+            panic!(
+                "ERROR: expected keyword var, but received {}",
+                self.tokenizer.token
+            );
+        }
+        self.write_token();
 
-    pub fn compile_statements() {}
+        self.tokenizer.advance();
+        let allowed_keywords = vec!["int", "string", "bool"];
+        // TODO: missing className
+        if self.tokenizer.token_type() != TokenType::Keyword
+            && !allowed_keywords.contains(&&self.tokenizer.token.as_str())
+        {
+            panic!(
+                "Error encountered. Expected a variable type, but received: {}",
+                self.tokenizer.token
+            );
+        }
+        self.write_token();
 
-    pub fn compile_let() {}
+        self._compile_identifier(
+            format!(
+                "ERROR: expected a variable name identifier, but received {}.",
+                self.tokenizer.token
+            )
+            .as_str(),
+        );
 
-    pub fn compile_if() {}
+        loop {
+            self.tokenizer.advance();
+            if self.tokenizer.token_type() == TokenType::Symbol && self.tokenizer.token == ";" {
+                self.write_token();
+                break;
+            }
 
-    pub fn compile_while() {}
+            if !(self.tokenizer.token_type() == TokenType::Symbol && self.tokenizer.token == ",") {
+                panic!(
+                    "ERROR: expected the symbol ',' comma, but received: {}",
+                    self.tokenizer.token
+                );
+            }
+
+            self.tokenizer.advance();
+            self._compile_identifier(
+                format!(
+                    "ERROR: expected a variable name identifier, but received {}.",
+                    self.tokenizer.token
+                )
+                .as_str(),
+            );
+        }
+        self.xml_writer.write_full_tag("</varDec>");
+    }
+
+    pub fn compile_statements(&mut self) {
+        self.tokenizer.advance();
+        if self.tokenizer.token_type() != TokenType::Keyword {
+            panic!("ERROR: a statement must include a keyword: let, if, while, do, return. Received {}.", self.tokenizer.token);
+        }
+
+        match self.tokenizer.token.as_str() {
+            "let" => {
+                self.compile_let();
+            }
+            "if" => {
+                self.compile_if();
+            }
+            "while" => {
+                self.compile_while();
+            }
+            "do" => {
+                self.compile_do();
+            }
+            "return" => {
+                self.compile_return();
+            }
+            _ => {
+                panic!("ERROR: unregocognized statement keyword!");
+            }
+        }
+    }
+
+    pub fn compile_let(&mut self) {
+        self.xml_writer.write_full_tag("<letStatement>");
+        self.xml_writer.write_full_tag("</letStatement>");
+    }
+
+    pub fn compile_if(&mut self) {
+        self.xml_writer.write_full_tag("<ifStatement>");
+        self.xml_writer.write_full_tag("</ifStatement>");
+    }
+
+    pub fn compile_while(&mut self) {
+        self.xml_writer.write_full_tag("<whileStatement>");
+        self.xml_writer.write_full_tag("</whileStatement>");
+    }
 
     pub fn compile_do(&mut self) {
+        self.xml_writer.write_full_tag("<doStatement>");
         self.write_token();
         // TODO: subroutineCall
+        self.xml_writer.write_full_tag("</doStatement>");
     }
 
     pub fn compile_return(&mut self) {
-        // Return
+        self.xml_writer.write_full_tag("<returnStatement>");
+
         self.write_token();
 
         self.tokenizer.advance();
         if self.tokenizer.token_type() != TokenType::Symbol {
             self.compile_expression();
+
+            self.tokenizer.advance();
+            if self.tokenizer.token_type() != TokenType::Symbol {
+                panic!("ERROR: expected a semi-colon, but received {}.", self.tokenizer.token); 
+            }
         }
 
         if self.tokenizer.token != ";" {
@@ -229,15 +334,27 @@ impl<'a> CompilationEngine<'a> {
                 self.tokenizer.token
             );
         }
-
         self.write_token();
+
+        self.xml_writer.write_full_tag("</returnStatement>");
     }
 
-    pub fn compile_expression(&mut self) {}
+    pub fn compile_expression(&mut self) {
+        self.xml_writer.write_full_tag("<expression>");
+        self.write_token();
+        // TODO: make it sane pls
+        self.xml_writer.write_full_tag("</expression>");
+    }
 
-    pub fn compile_term() {}
+    pub fn compile_term(&mut self) {
+        self.xml_writer.write_full_tag("<term>");
+        self.xml_writer.write_full_tag("</term>");
+    }
 
-    pub fn compile_expression_list() {}
+    pub fn compile_expression_list(&mut self) {
+        self.xml_writer.write_full_tag("<expressionList>");
+        self.xml_writer.write_full_tag("</expressionList>");
+    }
 
     fn _compile_identifier(&mut self, message: &str) {
         self.tokenizer.advance();
